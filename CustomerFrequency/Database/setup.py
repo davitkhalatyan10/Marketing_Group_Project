e9ae74df73b4918484c0e67b770386a1a6edd6fb
manestepanyan0109 <manestepanyan0301@gmail.com>
2024-04-07 00:10:12 +0400

    Delete setup.py

--- CustomerFrequency/Database/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    author='Group2',
-    description='CustomerFrequencyAnalysis',
-    name='CustomerFrequency',
-    version='0.1.0',
-    packages=find_packages(include=['CustomerFrequency', 'CustomerFrequency.*'])
-)
