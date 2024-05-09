aa79c572d83ab7d06612b979ec0c5f529bfb111f
davitkhalatyan10 <161630896+davitkhalatyan10@users.noreply.github.com>
2024-05-09 12:15:58 +0400

    fixing api.py and related files

--- CustomerFrequency/Api/api.py
+++ CustomerFrequency/Api/api.py
@@ -1,48 +1,32 @@
-from fastapi import FastAPI #, HTTPException
-#from fastapi.responses import FileResponse
-#import serial
-#from pathlib import Path
+import twilio.base.exceptions
+from fastapi import FastAPI
+from twilio.rest import Client
 import pandas as pd
+from pydantic import BaseModel
 from ..DataBase import sql_interactions as sqlint
+
+
+class SMSBody(BaseModel):
+    to: str
+    message: str
 
 
 app = FastAPI()
 dbname = 'temp'
-#ser = serial.Serial('COM1', 9600, timeout=1)
-'''
+
+
 @app.post("/send-sms/")
-async def send_sms(phone_number: str, message: str):
-    # Initialize the modem by sending AT command
-    ser.write(b'AT\r\n')
-    response = ser.readline().decode('utf-8').strip()
-    if response != 'OK':
-        raise HTTPException(status_code=500, detail="Failed to initialize modem")
-
-    # Set the SMS text mode
-    ser.write(b'AT+CMGF=1\r\n')
-    response = ser.readline().decode('utf-8').strip()
-    if response != 'OK':
-        raise HTTPException(status_code=500, detail="Failed to set SMS text mode")
-
-    # Send SMS command
-    ser.write(f'AT+CMGS="{phone_number}"\r\n'.encode('utf-8'))
-    response = ser.readline().decode('utf-8').strip()
-    if response != '>':
-        raise HTTPException(status_code=500, detail="Failed to send SMS command")
-
-    # Send SMS message
-    ser.write(f'{message}\r\n'.encode('utf-8'))
-    ser.write(bytes([26]))  # CTRL+Z to indicate end of message
-    response = ser.readlines()
-    if b'OK' not in response:
-        raise HTTPException(status_code=500, detail="Failed to send SMS")
-
+async def send_sms(sms_body: SMSBody):
+    account_sid = 'ACd07cbd09bfc3f448433e76786ccaae8f'
+    auth_token = '52b02c8db8ca00e2717ff32c9b37b694'
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=sms_body.message,
+        from_='+14352144650',
+        to=sms_body.to
+    )
     return {"status": "Message sent successfully"}
 
-@app.on_event("shutdown")
-def shutdown_event():
-    ser.close()
-'''
 
 @app.get("/")
 async def root():
@@ -73,7 +57,7 @@ async def get_visualization3():
 async def reach_lowest_average_visit_frequency(n: int):
     '''
     Calculate visits per day for each customer that have purchased our products more than once.
-     Then send a message to them.
+     Then send a message to the n customers who have the worst frequency.
     '''
     orders = sqlint.SqlHandler(dbname, 'orders')
     counts, differences = orders.average_visit_frequency()
@@ -84,8 +68,11 @@ async def reach_lowest_average_visit_frequency(n: int):
             frequencies.append((avg, count[1], count[2]))
     lowest = sorted(frequencies, key=lambda x: x[0])[:n]
     status = {'message': 'SMS not sent...'}
-    #for customer in lowest:
-        #status = send_sms(customer[2], "You rarely visit us but we remember you...")
+    for customer in lowest:
+        try:
+            status = await send_sms(SMSBody(to=customer[2], message="You rarely visit us but we remember you..."))
+        except twilio.base.exceptions.TwilioRestException:
+            pass
     return status
 
 @app.get("/get_data/no_visit")
@@ -97,27 +84,33 @@ async def attract_no_visit_n_days(n: int):
     customers = orders.no_visits_n_days(n)
     result = []
     status = {'message': 'SMS not sent...'}
-    #for row in customers:
-       # name = row[1] + ' ' + row[2]
-       # result.append((row[0], name))
-       # status = send_sms(row[3], "We have got free cookies for you!!!")
+    for row in customers:
+        name = row[1] + ' ' + row[2]
+        result.append((row[0], name))
+        try:
+            status = await send_sms(SMSBody(to=row[3], message="We have got free cookies for you!!!"))
+        except twilio.base.exceptions.TwilioRestException:
+            pass
 
     return status
 
-@app.get("/get_data/top_customers/{n}")
+@app.get("/get_data/top_customers")
 async def appreciate_top_visits(n: int):
     '''
-    Return top n of customers with the highest visit frequency.
+    Return top n of customers with the highest number of visits.
     '''
     orders = sqlint.SqlHandler(dbname, 'orders')
     visits = orders.top_visits()
     top = visits[:n]
-    result = {}
+    #result = {}
     status = {'message': 'SMS not sent...'}
-    #for row in top:
-       # name = row[2] + ' ' + row[3]
+    for row in top:
+        #name = row[2] + ' ' + row[3]
         #result[row[1]] = (name, row[0])
-        #status = send_sms(row[3], "We are happy that you are our customer!!!")
+        try:
+            status = await send_sms(SMSBody(to=row[4], message="We are happy that you are our customer!!!"))
+        except twilio.base.exceptions.TwilioRestException:
+            pass
 
     return status
 
@@ -128,11 +121,15 @@ async def bestseller_notification():
     '''
     orders = sqlint.SqlHandler(dbname, 'orders')
     top = orders.bestseller()
-    bestseller = {top[0]: [top[1], top[2]]}
     phones = orders.phone_numbers()
     status = {'message': 'SMS not sent...'}
-    #for phone in phones:
-        #status = send_sms(phone, f"If you have not tried our {top[2]} {top[1]}, then it is time to do it right now!!!")
+    for phone in phones:
+        try:
+            status = await send_sms(
+                SMSBody(to=phone,
+                        message=f"If you have not tried our {top[2]} {top[1]}, then it is time to do it right now!!!"))
+        except twilio.base.exceptions.TwilioRestException:
+            pass
 
     return status
 
